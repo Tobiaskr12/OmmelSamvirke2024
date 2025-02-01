80419a54706c11fcf1ad3f46313ff529f6c77d84
Tobias Kristensen <tobiaskristensen12@gmail.com>
2025-02-01 16:16:41 +0100

    Add EF migrations to version control

---
+++ Src/OmmelSamvirke.DataAccess/Migrations/20250201143220_AddContactListUnsubscriptionEntity.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OmmelSamvirke.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddContactListUnsubscriptionEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContactListUnsubscriptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UndoToken = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactListUnsubscriptions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DailyContactListAnalytics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContactListName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TotalContacts = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsNewsletter = table.Column<bool>(type: "bit", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DailyContactListAnalytics", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DailyEmailAnalytics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SentEmails = table.Column<int>(type: "int", nullable: false),
+                    TotalRecipients = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DailyEmailAnalytics", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContactListUnsubscriptions");
+
+            migrationBuilder.DropTable(
+                name: "DailyContactListAnalytics");
+
+            migrationBuilder.DropTable(
+                name: "DailyEmailAnalytics");
+        }
+    }
+}
