78c8f5a741414585625dbca847a0cf5703e9e1f5
Tobias Kristensen <tobiaskristensen12@gmail.com>
2025-02-16 19:46:05 +0100

    Add templates for the emails used by the system so far

---
+++ Contracts/Emails/EmailTemplateEngine/Templates.cs
@@ -0,0 +1,17 @@
+namespace ServiceModules.Emails.EmailTemplateEngine;
+
+public  static class Templates
+{
+    public static class General
+    {
+        public const string CriticalError = "CriticalError.html";
+        public const string EmailServiceLimitAlert = "EmailServiceLimitAlert.html";
+    }
+    
+    public static class ContactLists
+    {
+        public const string RequestUnsubscribe = "ContactLists/RequestUnsubscribe.html";
+        public const string UserRemovedFromContactList = "ContactLists/UserRemovedFromContactList.html";
+        
+    }
+}
